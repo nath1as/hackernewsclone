e5cbeff0e04cf0a78e621be99495ce82d492f4ba
nathias <n@th1.as>
2018-09-21 21:10:07 +0200

    controlled components

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ class App extends Component {
         <form>
           <input
             type="text"
+            value={searchTerm}
             onChange={this.onSearchChange}
           />
         </form>
