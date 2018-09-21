27e756e90c7d1aa874229503367245b105f01690
nathias <n@th1.as>
2018-09-21 17:08:41 +0200

    onDismiss not working

--- src/App.js
+++ src/App.js
@@ -28,12 +28,20 @@ class App extends Component {
     this.state = {
       list,
     };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDimiss(id)  {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.list.map((item) =>
+        {this.state.list.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}> {item.title} </a>
@@ -41,6 +49,14 @@ class App extends Component {
             <span> {item.author} </span>
             <span> {item.num_comments} </span>
             <span> {item.points} </span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)} 
+                type="button" 
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
