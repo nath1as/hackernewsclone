2abed19aee96d0a085669a68fdc6a615e521aee0
nathias <n@th1.as>
2018-09-22 12:07:45 +0200

    refactored all class components into arrow functions with implicit returns and destructured props in parameters

--- src/App.js
+++ src/App.js
@@ -76,49 +76,32 @@ const Search = ({ value, onChange, children }) =>
     />
   </form>
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}> {item.title} </a>
-            </span>
-            <span> {item.author} </span>
-            <span> {item.num_comments} </span>
-            <span> {item.points} </span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)} >
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        )}
+const Table = ({ list, pattern, onDismiss }) =>
+  <div>
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}> {item.title} </a>
+        </span>
+        <span> {item.author} </span>
+        <span> {item.num_comments} </span>
+        <span> {item.points} </span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)} >
+            Dismiss
+          </Button>
+        </span>
       </div>
-    );
-  }
-}
+    )}
+  </div>
 
-class Button extends Component {
-  render() {
-    const {
-      onClick,
-      className = '',
-      children,
-    } = this.props;
-
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-    );
-  }
-}
+const Button = ( {onClick, className = '', children} ) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
 
 export default App;
