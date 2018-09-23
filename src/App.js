af698115982b01c013a83f23343fe6596e2fbe79
nathias <n@th1.as>
2018-09-23 16:54:39 +0200

    refactored the if statement into a ternary for conditional rendering

--- src/App.js
+++ src/App.js
@@ -77,8 +77,6 @@ class App extends Component {
   render() {
     const { searchTerm, result } = this.state;
 
-    if (!result) { return null; }
-
     return (
       <div className="page">
         <div className="interactions">
@@ -89,11 +87,14 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result
+          ? <Table
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+          : null
+        }
       </div>
     );
   }
