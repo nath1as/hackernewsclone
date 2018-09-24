1d7d60e0a67875b757aa0d9e28bc432cc96eea9b
nathias <n@th1.as>
2018-09-24 15:26:21 +0200

    added table snaptest

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App, { Search, Button } from './App';
+import App, { Search, Button, Table } from './App';
 
 describe('Search', () => {
 
@@ -39,6 +39,30 @@ describe('Button', () => {
 
 });
 
+describe('Table', () => {
+  
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '1', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+  };
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props } />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Table { ...props } />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
 describe('App', () => {
 
   it('renders without crashing', () => {
@@ -54,5 +78,4 @@ describe('App', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
-
 });
