ec7cbff8f094451a8062391c9975f7c1e90f8c18
nathias <n@th1.as>
2018-09-24 15:18:52 +0200

    button snaptest added

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App, { Search } from './App';
+import App, { Search, Button } from './App';
 
 describe('Search', () => {
 
@@ -14,6 +14,24 @@ describe('Search', () => {
   test('has a valid snapshot', () => {
     const component = renderer.create(
       <Search>Search</Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+ 
+describe('Button', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Give Me More</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Button>Give Me More</Button>
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
