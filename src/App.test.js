52a02a19beae6402d2f4ae8b8d424db15fd341b1
nathias <n@th1.as>
2018-09-24 15:16:58 +0200

    search component snaptest added

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from './App';
+import App, { Search } from './App';
+
+describe('Search', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
 
 describe('App', () => {
 
